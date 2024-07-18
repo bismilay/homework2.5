e19037c1e247ea7ca5e950e7a739a16b528a6bb0
bismilay <iron-242004@mail.ru>
2024-07-18 11:22:44 +0300

    Add files via upload

---
+++ homework2,5.py
@@ -0,0 +1,11 @@
+homeworks='всего задач'
+the_number_of_complete_homeworks=12
+hours=',затрачено часов'
+the_number_of_hours_spent=1.5
+name1=the_number_of_hours_spent/the_number_of_complete_homeworks
+print(name1)
+time=',среднее время выполнения'
+name1=the_number_of_hours_spent/the_number_of_complete_homeworks
+name='Курc:Python,'
+print(name, 'всего задач', the_number_of_complete_homeworks , hours , the_number_of_hours_spent , time , name1 )
+# В принте попробовал оба варианта записи расшифровки переменной и название самой переменной , получилось^)
